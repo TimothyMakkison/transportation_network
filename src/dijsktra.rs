b6468c2bf132a6b17af790d9a67856da01166ec0
Timothy Makkison <timmakkison@gmail.com>
2021-08-20 23:54:53 +0100

    Start dijkstra

---
+++ src/dijsktra.rs
@@ -0,0 +1,20 @@
+use core::num::bignum::Big32x40;
+use std::{
+    collections::{BinaryHeap, HashMap},
+    hash::Hash,
+};
+
+use crate::graph::{Graph, NodeIndex};
+
+pub fn dijkstra<E, F>(
+    graph: Graph<E, F>,
+    start: NodeIndex,
+    goal: Option<NodeIndex>,
+) -> HashMap<NodeIndex, i32> {
+    let mut unvisited: BinaryHeap = BinaryHeap::new();
+
+    let mut distances: HashMap<NodeIndex, i32> = HashMap::new();
+    distances.insert(start, 0);
+
+    distances
+}
