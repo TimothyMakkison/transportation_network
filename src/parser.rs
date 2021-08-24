8cd5ed6a15920d92c6a843dd54c7ccd9577bb68e
Timothy Makkison <timmakkison@gmail.com>
2021-08-24 15:00:46 +0100

    Add parsers for check, findroute, findfastestroute,

--- src/parser.rs
+++ src/parser.rs
@@ -1,11 +1,25 @@
 use nom::character::complete::i32;
 use nom::character::{is_digit, is_space};
+use nom::multi::separated_list0;
 use nom::{
     branch::alt,
     bytes::complete::{tag, take_while},
     sequence::tuple,
     IResult,
 };
+
+fn parse_int(i: &[u8]) -> IResult<&[u8], i32> {
+    let (remainder, digits) = take_while(is_digit)(i)?;
+    let (_, integer) = i32(digits)?;
+
+    return Ok((remainder, integer));
+}
+
+fn parse_space(i: &[u8]) -> IResult<&[u8], &[u8]> {
+    let (remainder, spaces) = take_while(is_space)(i)?;
+
+    return Ok((remainder, spaces));
+}
 
 fn parse_maxdist(i: &[u8]) -> IResult<&[u8], Command> {
     let (a, _) = tag("MaxDist")(i)?;
@@ -18,27 +32,54 @@ fn parse_maxlink(i: &[u8]) -> IResult<&[u8], Command> {
 }
 
 fn parse_finddist(i: &[u8]) -> IResult<&[u8], Command> {
-    let space = take_while(is_space);
-
-    let (input, (_, a, _, b)) = tuple((
-        tag("FindDist "),
-        take_while(is_digit),
-        space,
-        take_while(is_digit),
-    ))(i)?;
-
-    let (_, a) = i32(a)?;
-    let (_, b) = i32(b)?;
+    let (input, (_, a, _, b)) = tuple((tag("FindDist "), parse_int, parse_space, parse_int))(i)?;
 
     Ok((input, Command::FindDist(a, b)))
 }
 
 fn parse_findneighbours(i: &[u8]) -> IResult<&[u8], Command> {
-    let (input, (_, a)) = tuple((tag("FindNeighbour "), take_while(is_digit)))(i)?;
-
-    let (_, a) = i32(a)?;
+    let (input, (_, a)) = tuple((tag("FindNeighbour "), parse_int))(i)?;
 
     Ok((input, Command::FindNeighbour(a)))
+}
+
+fn parse_places(i: &[u8]) -> IResult<&[u8], Vec<i32>> {
+    let (a, b) = separated_list0(tag(" "), parse_int)(i)?;
+
+    return Ok((a, b));
+}
+
+fn parse_check(i: &[u8]) -> IResult<&[u8], Command> {
+    let (input, (_, mode, _, integers)) =
+        tuple((tag("Check "), parse_mode, parse_space, parse_places))(i)?;
+
+    Ok((input, Command::Check(mode, integers)))
+}
+
+fn parse_findroute(i: &[u8]) -> IResult<&[u8], Command> {
+    let (input, (_, mode, _, a, _, b)) = tuple((
+        tag("FindRoute "),
+        parse_mode,
+        parse_space,
+        parse_int,
+        parse_space,
+        parse_int,
+    ))(i)?;
+
+    Ok((input, Command::FindRoute(mode, a, b)))
+}
+
+fn parse_findfastestroute(i: &[u8]) -> IResult<&[u8], Command> {
+    let (input, (_, mode, _, a, _, b)) = tuple((
+        tag("FindShortestRoute "),
+        parse_mode,
+        parse_space,
+        parse_int,
+        parse_space,
+        parse_int,
+    ))(i)?;
+
+    Ok((input, Command::FindRoute(mode, a, b)))
 }
 
 pub fn parse_command(input: &[u8]) -> IResult<&[u8], Command> {
@@ -47,6 +88,9 @@ pub fn parse_command(input: &[u8]) -> IResult<&[u8], Command> {
         parse_maxlink,
         parse_finddist,
         parse_findneighbours,
+        parse_check,
+        parse_findroute,
+        parse_findfastestroute,
     ))(input)
 }
 
@@ -101,3 +145,17 @@ fn parse_ship(i: &[u8]) -> IResult<&[u8], TravelMode> {
     let (a, _) = tag("Ship")(i)?;
     Ok((a, TravelMode::Ship))
 }
+
+// macro_rules! mode_parser {
+//     ($($t:tt, $y:ty)+) => {
+//         $(
+//         pub fn $t(input: &[u8]) -> IResult<&[u8], $t>
+//             {
+//                 let (a, _) = tag($t)(input)?;
+//                 Ok((a, $y))
+//             }
+//         )+
+//     }
+// }
+
+// mode_parser! {TransportType::Foot}
