cf9a4c9d7251211b77ff223ec764873d92321a06
Timothy Makkison <timmakkison@gmail.com>
2021-08-24 01:15:01 +0100

    Add find_neighbour and mode parsers.

--- src/parser.rs
+++ src/parser.rs
@@ -1,8 +1,8 @@
+use nom::character::complete::i32;
 use nom::character::{is_digit, is_space};
 use nom::{
     branch::alt,
     bytes::complete::{tag, take_while},
-    number::complete::be_i32,
     sequence::tuple,
     IResult,
 };
@@ -19,6 +19,7 @@ fn parse_maxlink(i: &[u8]) -> IResult<&[u8], Command> {
 
 fn parse_finddist(i: &[u8]) -> IResult<&[u8], Command> {
     let space = take_while(is_space);
+
     let (input, (_, a, _, b)) = tuple((
         tag("FindDist "),
         take_while(is_digit),
@@ -26,14 +27,33 @@ fn parse_finddist(i: &[u8]) -> IResult<&[u8], Command> {
         take_while(is_digit),
     ))(i)?;
 
-    let (_, a) = be_i32(a)?;
-    let (_, b) = be_i32(b)?;
+    let (_, a) = i32(a)?;
+    let (_, b) = i32(b)?;
 
     Ok((input, Command::FindDist(a, b)))
 }
 
-pub fn parser(input: &[u8]) -> IResult<&[u8], Command> {
-    alt((parse_maxdist, parse_maxlink, parse_finddist))(input)
+fn parse_findneighbours(i: &[u8]) -> IResult<&[u8], Command> {
+    let (input, (_, a)) = tuple((tag("FindNeighbour "), take_while(is_digit)))(i)?;
+
+    let (_, a) = i32(a)?;
+
+    Ok((input, Command::FindNeighbour(a)))
+}
+
+pub fn parse_command(input: &[u8]) -> IResult<&[u8], Command> {
+    alt((
+        parse_maxdist,
+        parse_maxlink,
+        parse_finddist,
+        parse_findneighbours,
+    ))(input)
+}
+
+pub fn parse_mode(input: &[u8]) -> IResult<&[u8], TravelMode> {
+    alt((
+        parse_foot, parse_bike, parse_rail, parse_car, parse_bus, parse_ship,
+    ))(input)
 }
 
 #[derive(Debug)]
@@ -50,8 +70,34 @@ pub enum Command {
 pub enum TravelMode {
     Foot,
     Bike,
-    Rail,
     Car,
     Bus,
     Ship,
+    Rail,
+}
+
+fn parse_foot(i: &[u8]) -> IResult<&[u8], TravelMode> {
+    let (a, _) = tag("Foot")(i)?;
+    Ok((a, TravelMode::Foot))
+}
+
+fn parse_bike(i: &[u8]) -> IResult<&[u8], TravelMode> {
+    let (a, _) = tag("Bike")(i)?;
+    Ok((a, TravelMode::Bike))
+}
+fn parse_rail(i: &[u8]) -> IResult<&[u8], TravelMode> {
+    let (a, _) = tag("Rail")(i)?;
+    Ok((a, TravelMode::Rail))
+}
+fn parse_car(i: &[u8]) -> IResult<&[u8], TravelMode> {
+    let (a, _) = tag("Car")(i)?;
+    Ok((a, TravelMode::Car))
+}
+fn parse_bus(i: &[u8]) -> IResult<&[u8], TravelMode> {
+    let (a, _) = tag("Bus")(i)?;
+    Ok((a, TravelMode::Bus))
+}
+fn parse_ship(i: &[u8]) -> IResult<&[u8], TravelMode> {
+    let (a, _) = tag("Ship")(i)?;
+    Ok((a, TravelMode::Ship))
 }
