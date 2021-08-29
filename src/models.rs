b2b24d5e2683df5b6348d428d605a634cf95ad20
Timothy Makkison <timmakkison@gmail.com>
2021-08-29 13:02:08 +0100

    Implement Display for Place

--- src/models.rs
+++ src/models.rs
@@ -1,7 +1,7 @@
 use serde::Deserialize;
 use std::fmt::Display;
 
-#[derive(Debug, PartialEq, Deserialize)]
+#[derive(Debug, PartialEq, Deserialize, Clone, Copy)]
 pub enum TravelMode {
     Foot,
     Bike,
@@ -17,19 +17,41 @@ impl Display for TravelMode {
     }
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone, Copy)]
 pub struct Link {
     start: i32,
     end: i32,
     mode: TravelMode,
 }
 
-#[derive(Debug, Deserialize)]
+#[derive(Debug, Deserialize, Clone)]
 pub struct Place {
-    name: String,
-    id: i32,
+    pub name: String,
+    pub id: i32,
     latitude: f32,
     longitude: f32,
+}
+
+impl Place {
+    pub fn new(name: String, id: i32, latitude: f32, longitude: f32) -> Self {
+        Place {
+            name,
+            id,
+            latitude,
+            longitude,
+        }
+    }
+
+    /// Get a reference to the place's id.
+    pub fn id(&self) -> &i32 {
+        &self.id
+    }
+}
+
+impl Display for Place {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{:?}", self)
+    }
 }
 
 #[derive(Debug)]
