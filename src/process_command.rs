471ef132eb94d73ae69896f88476c1a720b72a80
Timothy Makkison <timmakkison@gmail.com>
2021-08-30 18:32:41 +0100

    Add find_shortest_route function

--- src/process_command.rs
+++ src/process_command.rs
@@ -16,26 +16,34 @@ pub fn process(
         Command::FindNeighbour(place) => place.to_string(),
         Command::FindShortestRoute(_, _, _) => find_shortest_route(&graph, node_map, command),
         _ => "Not impl".to_string(),
+        Command::MaxLink => todo!(),
+        Command::FindDist(_, _) => todo!(),
+        Command::Check(_, _) => todo!(),
+        Command::FindRoute(_, _, _) => todo!(),
     }
 }
 
-fn find_shortest_route<N, E>(
-    graph: &Graph<N, E>,
+fn find_shortest_route(
+    graph: &Graph<PlaceCopy, Link>,
     id_map: &HashMap<i32, NodeIndex>,
     command: Command,
-) -> String
-where
-    N: Display + Copy,
-{
+) -> String {
     if let Command::FindShortestRoute(mode, start, goal) = command {
         let start_node = id_map.get(&start).unwrap();
-        let goal_node = id_map.get(&start).unwrap();
+        let goal_node = id_map.get(&goal).unwrap();
 
-        let mut output = format!("FindShortestRoute {} {} {}", mode, start, goal);
-        let routes = dijkstra(&graph, *start_node, Some(*goal_node), |_| 1, |x| true);
+        let routes = dijkstra(
+            &graph,
+            *start_node,
+            Some(*goal_node),
+            |_| 1,
+            |x| can_traverse(&mode, &x.mode),
+        );
 
         let mut nodes = vec![goal_node];
         let mut curr = goal_node;
+
+        let mut output = format!("FindShortestRoute {} {} {}", mode, start, goal);
 
         while curr != start_node {
             match routes.get(curr) {
@@ -44,19 +52,21 @@ where
                     nodes.push(curr);
                 }
                 None => {
-                    output.push_str("\n Fail");
+                    output = format!("{} \nFail", output);
+
                     return output;
                 }
             }
         }
         nodes.reverse();
 
+        println!("{:?}", nodes);
         for i in nodes {
             let node = graph.get_node(*i).unwrap();
-            output.push_str(&node.data.to_string());
+            output = format!("{}\n{}", output, &node.data.to_string());
         }
 
-        output.to_string()
+        output
     } else {
         panic!();
     }
