cab02442c4171dc84c1c311915c1bd70481e8594
Timothy Makkison <timmakkison@gmail.com>
2021-09-02 22:10:19 +0100

    Write benchmarks for building, reading and full run

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,14 +1,15 @@
 use criterion::{black_box, criterion_group, criterion_main, Criterion};
 
-use learning_graph::algorithms::graph::{Graph, NodeIndex};
-use learning_graph::models::Link;
-use learning_graph::models::Place;
+use learning_graph::models::Command;
 use learning_graph::process_command::CommandProcessor;
 use learning_graph::startup::deserialization::{read_commands, read_links, read_places};
+use learning_graph::startup::graph_builder;
 
-use std::collections::HashMap;
+fn run(processor: CommandProcessor, commands: Vec<Command>) {
+    let _: Vec<String> = commands.into_iter().map(|x| processor.process(x)).collect();
+}
 
-fn run() {
+fn build() -> (CommandProcessor, Vec<Command>) {
     let places_path = "Places.csv";
     let links_path = "Links.csv";
     let command_path = "Commands.txt";
@@ -16,44 +17,41 @@ fn run() {
     let nodes = read_places(places_path);
     let links = read_links(links_path);
 
-    let (graph, map) = build_graph(nodes, links);
-
-    let processor = CommandProcessor::new(graph, map);
+    let (graph, map) = graph_builder::build(nodes, links);
 
     let commands = read_commands(command_path).unwrap();
 
-    let results: Vec<String> = commands.into_iter().map(|x| processor.process(x)).collect();
-
-    for result in results {
-        println!("{}\n", result);
-    }
-}
-
-fn build_graph(
-    nodes: Vec<Place>,
-    edges: Vec<Link>,
-) -> (Graph<Place, Link>, HashMap<i32, NodeIndex>) {
-    let mut graph = Graph::new();
-    let mut map = HashMap::new();
-
-    for node in nodes {
-        let id = node.id;
-        let node_index = graph.add_node(node);
-        map.insert(id, node_index);
-    }
-
-    for edge in edges {
-        let a = map.get(&edge.start).unwrap();
-        let b = map.get(&edge.end).unwrap();
-        graph.add_edge(*a, *b, edge);
-    }
-
-    (graph, map)
+    let processor = CommandProcessor::new(graph, map);
+    (processor, commands)
 }
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("run 20", |b| b.iter(|| run()));
+    c.bench_function("run-all-commands", |b| {
+        let (proc, commands) = build();
+
+        b.iter(|| {
+            run(black_box(proc.clone()), black_box(commands.clone()));
+        })
+    });
+
+    c.bench_function("build-graph", |b| {
+        b.iter(|| {
+            build();
+        })
+    });
+
+    c.bench_function("build-and-run-all-commands", |b| {
+        b.iter(|| {
+            let (proc, commands) = build();
+            run(black_box(proc.clone()), black_box(commands.clone()));
+        })
+    });
 }
 
-criterion_group!(benches, criterion_benchmark);
+criterion_group! {
+    name = benches;
+    // This can be any expression that returns a `Criterion` object.
+    config = Criterion::default().significance_level(0.1).sample_size(500);
+    targets = criterion_benchmark
+}
 criterion_main!(benches);
