36fcab1402a29a394acdbdfc939d3c149ab974a6
Timothy Makkison <timmakkison@gmail.com>
2022-06-06 21:55:37 +0100

    Edit update pacakges

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,3 @@
-use std::collections::HashMap;
-
 use learning_graph::process_command::CommandProcessor;
 use learning_graph::startup::deserialization::{read_commands, read_links, read_places};
 use learning_graph::startup::graph_builder::{self};
@@ -11,16 +9,6 @@ fn main() {
 
     let nodes = read_places(places_path);
     let links = read_links(links_path);
-
-    let mut counter = HashMap::new();
-    for link in links.clone() {
-        counter
-            .entry(link.mode)
-            .and_modify(|x| *x += 1)
-            .or_insert(1);
-    }
-
-    println!("{:?}", counter);
 
     let (graph, map) = graph_builder::build(nodes, links);
 
--- src/startup/parser.rs
+++ src/startup/parser.rs
@@ -10,55 +10,56 @@ use nom::{
 
 use crate::models::{Command, TravelMode};
 
-fn parse_int(i: &[u8]) -> IResult<&[u8], i32> {
-    let (remainder, digits) = take_while(is_digit)(i)?;
+fn parse_int(bytes: &[u8]) -> IResult<&[u8], i32> {
+    let (remainder, digits) = take_while(is_digit)(bytes)?;
     let (_, integer) = i32(digits)?;
 
     Ok((remainder, integer))
 }
 
-fn parse_space(i: &[u8]) -> IResult<&[u8], &[u8]> {
-    let (remainder, spaces) = take_while(is_space)(i)?;
+fn parse_space(bytes: &[u8]) -> IResult<&[u8], &[u8]> {
+    let (remainder, spaces) = take_while(is_space)(bytes)?;
 
     Ok((remainder, spaces))
 }
 
-fn parse_maxdist(i: &[u8]) -> IResult<&[u8], Command> {
-    let (a, _) = tag("MaxDist")(i)?;
+fn parse_maxdist(bytes: &[u8]) -> IResult<&[u8], Command> {
+    let (a, _) = tag("MaxDist")(bytes)?;
     Ok((a, Command::MaxDist))
 }
 
-fn parse_maxlink(i: &[u8]) -> IResult<&[u8], Command> {
-    let (a, _) = tag("MaxLink")(i)?;
+fn parse_maxlink(bytes: &[u8]) -> IResult<&[u8], Command> {
+    let (a, _) = tag("MaxLink")(bytes)?;
     Ok((a, Command::MaxLink))
 }
 
-fn parse_finddist(i: &[u8]) -> IResult<&[u8], Command> {
-    let (input, (_, a, _, b)) = tuple((tag("FindDist "), parse_int, parse_space, parse_int))(i)?;
+fn parse_finddist(bytes: &[u8]) -> IResult<&[u8], Command> {
+    let (input, (_, a, _, b)) =
+        tuple((tag("FindDist "), parse_int, parse_space, parse_int))(bytes)?;
 
     Ok((input, Command::FindDist(a, b)))
 }
 
-fn parse_findneighbours(i: &[u8]) -> IResult<&[u8], Command> {
-    let (input, (_, a)) = tuple((tag("FindNeighbour "), parse_int))(i)?;
+fn parse_findneighbours(bytes: &[u8]) -> IResult<&[u8], Command> {
+    let (input, (_, a)) = tuple((tag("FindNeighbour "), parse_int))(bytes)?;
 
     Ok((input, Command::FindNeighbour(a)))
 }
 
-fn parse_places(i: &[u8]) -> IResult<&[u8], Vec<i32>> {
-    let (a, b) = separated_list0(tag(" "), parse_int)(i)?;
+fn parse_places(bytes: &[u8]) -> IResult<&[u8], Vec<i32>> {
+    let (a, b) = separated_list0(tag(" "), parse_int)(bytes)?;
 
     Ok((a, b))
 }
 
-fn parse_check(i: &[u8]) -> IResult<&[u8], Command> {
+fn parse_check(bytes: &[u8]) -> IResult<&[u8], Command> {
     let (input, (_, mode, _, integers)) =
-        tuple((tag("Check "), parse_mode, parse_space, parse_places))(i)?;
+        tuple((tag("Check "), parse_mode, parse_space, parse_places))(bytes)?;
 
     Ok((input, Command::Check(mode, integers)))
 }
 
-fn parse_findroute(i: &[u8]) -> IResult<&[u8], Command> {
+fn parse_findroute(bytes: &[u8]) -> IResult<&[u8], Command> {
     let (input, (_, mode, _, a, _, b)) = tuple((
         tag("FindRoute "),
         parse_mode,
@@ -66,12 +67,12 @@ fn parse_findroute(i: &[u8]) -> IResult<&[u8], Command> {
         parse_int,
         parse_space,
         parse_int,
-    ))(i)?;
+    ))(bytes)?;
 
     Ok((input, Command::FindRoute(mode, a, b)))
 }
 
-fn parse_findfastestroute(i: &[u8]) -> IResult<&[u8], Command> {
+fn parse_findfastestroute(bytes: &[u8]) -> IResult<&[u8], Command> {
     let (input, (_, mode, _, a, _, b)) = tuple((
         tag("FindShortestRoute "),
         parse_mode,
@@ -79,7 +80,7 @@ fn parse_findfastestroute(i: &[u8]) -> IResult<&[u8], Command> {
         parse_int,
         parse_space,
         parse_int,
-    ))(i)?;
+    ))(bytes)?;
 
     Ok((input, Command::FindShortestRoute(mode, a, b)))
 }
@@ -104,32 +105,43 @@ pub fn parse_mode(input: &[u8]) -> IResult<&[u8], TravelMode> {
 
 //TODO Replace with macros
 
-fn parse_foot(i: &[u8]) -> IResult<&[u8], TravelMode> {
-    let (a, _) = tag("Foot")(i)?;
+fn parse_foot(bytes: &[u8]) -> IResult<&[u8], TravelMode> {
+    let (a, _) = tag("Foot")(bytes)?;
     Ok((a, TravelMode::Foot))
 }
-
-fn parse_bike(i: &[u8]) -> IResult<&[u8], TravelMode> {
-    let (a, _) = tag("Bike")(i)?;
+fn parse_bike(bytes: &[u8]) -> IResult<&[u8], TravelMode> {
+    let (a, _) = tag("Bike")(bytes)?;
     Ok((a, TravelMode::Bike))
 }
-fn parse_rail(i: &[u8]) -> IResult<&[u8], TravelMode> {
-    let (a, _) = tag("Rail")(i)?;
+fn parse_rail(bytes: &[u8]) -> IResult<&[u8], TravelMode> {
+    let (a, _) = tag("Rail")(bytes)?;
     Ok((a, TravelMode::Rail))
 }
-fn parse_car(i: &[u8]) -> IResult<&[u8], TravelMode> {
-    let (a, _) = tag("Car")(i)?;
+fn parse_car(bytes: &[u8]) -> IResult<&[u8], TravelMode> {
+    let (a, _) = tag("Car")(bytes)?;
     Ok((a, TravelMode::Car))
 }
-fn parse_bus(i: &[u8]) -> IResult<&[u8], TravelMode> {
-    let (a, _) = tag("Bus")(i)?;
+fn parse_bus(bytes: &[u8]) -> IResult<&[u8], TravelMode> {
+    let (a, _) = tag("Bus")(bytes)?;
     Ok((a, TravelMode::Bus))
 }
-fn parse_ship(i: &[u8]) -> IResult<&[u8], TravelMode> {
-    let (a, _) = tag("Ship")(i)?;
+fn parse_ship(bytes: &[u8]) -> IResult<&[u8], TravelMode> {
+    let (a, _) = tag("Ship")(bytes)?;
     Ok((a, TravelMode::Ship))
 }
 
+#[macro_export]
+macro_rules! mag {
+    ( $( $x:expr ),* ) => {
+        {
+            let mut temp_vec = Vec::new();
+            $(
+                temp_vec.push($x);
+            )*
+            temp_vec
+        }
+    };
+}
 // macro_rules! mode_parser {
 //     ($($t:tt, $y:ty)+) => {
 //         $(
