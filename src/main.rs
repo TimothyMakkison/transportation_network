ebd75a244008b287eadd844e1f42949ce8762b21
Timothy Makkison <timmakkison@gmail.com>
2022-06-06 21:59:23 +0100

    Edit update paths

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,18 @@ use learning_graph::startup::deserialization::{read_commands, read_links, read_p
 use learning_graph::startup::graph_builder::{self};
 
 fn main() {
-    let places_path = "Places.csv";
-    let links_path = "Links.csv";
-    let command_path = "Commands.txt";
+    const PLACES_PATH: &str = "Places.csv";
+    const LINKS_PATH: &str = "Links.csv";
+    const COMMAND_PATH: &str = "Commands.txt";
 
-    let nodes = read_places(places_path);
-    let links = read_links(links_path);
+    let nodes = read_places(PLACES_PATH);
+    let links = read_links(LINKS_PATH);
 
     let (graph, map) = graph_builder::build(nodes, links);
 
     let processor = CommandProcessor::new(graph, map);
 
-    let commands = read_commands(command_path).unwrap();
+    let commands = read_commands(COMMAND_PATH).unwrap();
 
     let results: Vec<String> = commands.into_iter().map(|x| processor.process(x)).collect();
 
